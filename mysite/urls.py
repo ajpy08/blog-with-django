d4838014b4adc430c28bb6669b9eb04faff6c439
I.S.C. Javi Puc <angelpuc08@gmail.com>
2016-01-26 15:24:36 -0600

    Agregue primera plantilla y modifique urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('blog.urls'), namespace='home'),
+    url('', include('blog.urls', namespace='home')),
 ]
